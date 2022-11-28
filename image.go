59deebbc111ca94b5a558dba69ed8c7ab6b27aec
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2022-11-28 16:16:25 +0300

    clean up full image code

--- image.go
+++ image.go
@@ -46,15 +46,24 @@ func heightCenteredImage(srtmImg *SRTMImage, height int16) *image.Gray {
 }
 
 // FullImage returns the full data range as a 16 bit-depth grayscale image.
-// Please note that some image viewers may not display the full range of values.
-// Or even display wrong values.
-func (s *SRTMImage) FullImage() *image.Gray16 {
-	rect := image.Rect(0, 0, s.Format.Size(), s.Format.Size())
+// This preserves the original data completely and allows for further processing.
+//
+// Please note that some image viewers are not able to display 16 bit images.
+// In many cases, the image will be displayed as a 8 bit image or will be displayed incorrectly.
+func (srtmImg *SRTMImage) FullImage() *image.Gray16 {
+	rect := image.Rect(0, 0, srtmImg.Format.Size(), srtmImg.Format.Size())
 	img := image.NewGray16(rect)
 
-	for i, v := range s.Data {
-		var converted uint16 = uint16((int32(v) + 32768) & 0xFFFF)
-		binary.BigEndian.PutUint16(img.Pix[i*2:i*2+2], converted)
+	for i, v := range srtmImg.Data {
+		// Gray16 uses uint16, but the data is int16, so we shift the values by 32768
+		// Gray16 uses big endian, according to its documentation
+		binary.BigEndian.PutUint16(img.Pix[i*2:i*2+2], signed16BitToUint16(v))
 	}
 	return img
+}
+
+// signed16BitToUint16 converts a signed 16 bit integer to an unsigned 16 bit integer.
+// It shifts the value by 32768. e.g. -32768 becomes 0, 0 becomes 32768, 32767 becomes 65535.
+func signed16BitToUint16(v int16) uint16 {
+	return uint16((int32(v) + 32768) & 0xFFFF)
 }
