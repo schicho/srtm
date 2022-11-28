0e41f1a8903eeb9035be51e9001bd1e00fa84146
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2022-11-28 16:58:58 +0300

    use points to access data

--- info.go
+++ info.go
@@ -4,29 +4,51 @@ import (
 	"image"
 )
 
-// CountDataVoids returns the number of voids in the data.
+// DataVoidIndices returns points of all voids in the srtmImage.
 // Data voids are represented by the value -32768 as per the SRTM documentation.
-func (s *SRTMImage) CountDataVoids() int {
-	var count = 0
-	for _, v := range s.Data {
-		if v == -32768 {
-			count++
-		}
-	}
-	return count
-}
-
-// DataVoidIndices returns the indices of the voids in the data.
-// Data voids are represented by the value -32768 as per the SRTM documentation.
-// The indices are returned as a index to the data array. Not as x,y coordinates.
-func (s *SRTMImage) DataVoidIndices() []int {
-	var indices = make([]int, 0)
+func (s *SRTMImage) DataVoidPoints() []image.Point {
+	var points []image.Point
 	for i, v := range s.Data {
 		if v == -32768 {
-			indices = append(indices, i)
+			points = append(points, s.IndexToCoordinates(i))
 		}
 	}
-	return indices
+	return points
+}
+
+// MinMaxElevation returns the minimum and maximum elevation values.
+// Values may be erroneous, because of voids or other invalid data.
+func (s *SRTMImage) MinMaxElevation() (min int16, max int16) {
+	// do not forget to initialize min and max
+	min = 32767
+	max = -32768
+
+	for _, v := range s.Data {
+		// avoid letting voids influence the min/max
+		if v < min && v != -32768 {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	return
+}
+
+// MeanElevation returns the mean elevation value.
+// Overflows are mitigated. As well as voids.
+// Thus may not be the actual mean.
+func (s *SRTMImage) MeanElevation() int16 {
+	var avg = 0
+
+	for i, v := range s.Data {
+		// avoid adding voids, by just adding the average
+		if v == -32768 {
+			v = int16(avg)
+		}
+		avg += (int(v) - avg) / (i + 1)
+	}
+	return int16(avg)
 }
 
 // IndexToCoordinates converts an index into the data array of the given SRTMImage
@@ -58,39 +80,4 @@ func CoordinatesToIndex(point image.Point, format SRTMFormat) int {
 // ElevationAt returns the elevation value at the given coordinates.
 func (s *SRTMImage) ElevationAt(x, y int) int16 {
 	return s.Data[s.CoordinatesToIndex(image.Point{x, y})]
-}
-
-// MinMaxElevation returns the minimum and maximum elevation values.
-// Values may be erroneous, because of voids or other invalid data.
-func (s *SRTMImage) MinMaxElevation() (min int16, max int16) {
-	// Do not forget to initialize min and max
-	min = 32767
-	max = -32768
-
-	for _, v := range s.Data {
-		// avoid letting voids influence the min/max
-		if v < min && v != -32768 {
-			min = v
-		}
-		if v > max {
-			max = v
-		}
-	}
-	return
-}
-
-// MeanElevation returns the mean elevation value.
-// Overflows are mitigated. As well as voids.
-// Thus may not be the actual mean.
-func (s *SRTMImage) MeanElevation() int16 {
-	var avg = 0
-
-	for i, v := range s.Data {
-		// avoid adding voids, by just adding the average
-		if v == -32768 {
-			v = int16(avg)
-		}
-		avg += (int(v) - avg) / (i + 1)
-	}
-	return int16(avg)
 }
